600ce0719809c6633e2367316b13d0f4d9364ef9
Lalit Shankar Chowdhury <lalitshankarch@gmail.com>
2026-04-20 21:54:14 +0530

    Use `riscv64-linux-gnu` toolchain

--- test/build.sh
+++ test/build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-riscv32-linux-gnu-as -march=rv32i -mabi=ilp32 hello.s -o hello.o
 
-riscv32-linux-gnu-ld -T link.ld hello.o -o hello.elf
+riscv64-linux-gnu-as -march=rv32i -mabi=ilp32 hello.s -o hello.o
 
-riscv32-linux-gnu-objcopy -O binary hello.elf hello.bin
+riscv64-linux-gnu-ld -m elf32lriscv -T link.ld hello.o -o hello.elf
+
+riscv64-linux-gnu-objcopy -O binary hello.elf hello.bin
